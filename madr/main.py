ef8a907621a03056895fefc83936a66c6cbd48b6
romario lima <romariolima1006@gmail.com>
2024-09-10 11:45:57 -0300

    metodos para listar e atualizar usuario

--- madr/main.py
+++ madr/main.py
@@ -6,13 +6,10 @@ url_login = 'http://127.0.0.1:8000/auth/token'
 url_users = 'http://127.0.0.1:8000/users'
 
 
-def login(url):
-
-    user = input('digite seu usuario: ')
-    password = input('digite sua senha: ')
+def login(url, data):
 
     request = make_request(
-        'post', url, data={"username": user, "password": password})
+        'post', url, data=data)
 
     response = request.json()
 
@@ -32,13 +29,43 @@ def create_user(method, url, data):
 
     response = request.json()
 
-    if request.status_code != 200:
+    if request.status_code != 201:
         print(response.get('detail'))
         return
 
     print('usuario criado com sucesso')
 
 
+def list_users(url, index):
+    url = url + f'?offset={index}'
+
+    request = make_request('get', url)
+
+    response = request.json()
+
+    if request.status_code != 200:
+        print(response.get('detail'))
+        return
+
+    return response
+
+
+def update_user(url, id, data, headers):
+    url = url + f'/{id}'
+    request = make_request('put', url, json=data, headers=headers)
+
+    response = request.json()
+
+    if request.status_code != 200:
+        print(response.get('detail'))
+        return
+
+    print(f'id: {response["id"]}')
+    print(f'username: {response["username"]}')
+    print(f'email: {response["email"]}')
+
+
+# ----------------------------------------------------------------------------
 if not token:
     while True:
 
@@ -52,7 +79,12 @@ if not token:
         os.system('clear')
 
         if value == '1':
-            token = login(url_login)
+            user = input('digite seu usuario: ')
+            password = input('digite sua senha: ')
+
+            data = {"username": user, "password": password}
+
+            token = login(url_login, data)
             if token:
                 break
 
@@ -73,6 +105,8 @@ if not token:
 
         else:
             print('opcao invalida')
+
+# ---------------------------------------------------------------------------------------
 
 if token:
     while True:
@@ -102,9 +136,47 @@ if token:
                 os.system('clear')
 
                 if value == '1':
-                    ...
+                    index = 0
+                    while True:
+                        users = list_users(url_users, index)
+                        for key, value in users.items():
+                            for user in value:
+                                print(
+                                    '######################################################### \n')
+                                print(f'id: {user["id"]}')
+                                print(f'username: {user["username"]}')
+                                print(f'email: {user["email"]}')
+                        print(
+                            '#############################################################')
+                        print('digite: s, para sair, ou qualquer tecla para proximo')
+
+                        value = input('digite a opcao desejada: ')
+                        if value.lower() == 's':
+                            break
+
+                        index += 1
+
                 elif value == '2':
-                    ...
+                    try:
+                        id = int(
+                            input('digite o id do usuario que deseja atualizar: '))
+                    except:
+                        print('digite apenas numeros')
+                        continue
+
+                    username = input('digite o username: ')
+                    email = input('digite seu email: ')
+                    password = input('digite a senha: ')
+
+                    data = {
+                        "username": username,
+                        "email": email,
+                        "password": password
+                    }
+
+                    headers = {"Authorization": token}
+
+                    update_user(url_users, id, data, headers)
                 elif value == '3':
                     ...
                 elif value.lower() == 's':
